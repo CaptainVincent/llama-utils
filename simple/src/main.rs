1f8917cbdfb653441c25e9c579b93feb4bd7b70b
vincent <vincent@secondstate.io>
2023-11-06 17:48:32 +0800

    workaround: enable stream output

--- simple/src/main.rs
+++ simple/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use wasi_nn;
+use serde_json::json;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,17 +14,22 @@ fn main() {
 
     let mut context = graph.init_execution_context().expect("Failed to init context");
     let tensor_data = prompt.as_bytes().to_vec();
+    let options = json!({
+        "stream-stdout": true,
+        "enable-log": true,
+    });
+    context
+        .set_input(
+            1,
+            wasi_nn::TensorType::U8,
+            &[1],
+            &options.to_string().as_bytes().to_vec(),
+        )
+        .unwrap();
     context
         .set_input(0, wasi_nn::TensorType::U8, &[1], &tensor_data)
         .expect("Failed to set prompt as the input tensor");
-    context.compute().expect("Failed to complete inference");
-
-    let max_output_size = 4096*6;
-    let mut output_buffer = vec![0u8; max_output_size];
-    let mut output_size = context.get_output(0, &mut output_buffer).expect("Failed to get inference output");
-    output_size = std::cmp::min(max_output_size, output_size);
-    let output = String::from_utf8_lossy(&output_buffer[..output_size]).to_string();
 
     println!("{}", prompt);
-    println!("{}", output);
+    context.compute().expect("Failed to complete inference");
 }
